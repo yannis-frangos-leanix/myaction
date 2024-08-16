420c34dff235f83e61de3851c3569e03def225b0
Yannis Frangos <yannis.frangos@leanix.net>
2024-08-16 16:14:33 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+async function run() {
+  try {
+    // Checkout repository
+    await exec.exec('git clone https://github.com/user/repo.git');
+
+    // Set up JDK
+    await exec.exec('wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -');
+    await exec.exec('echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list');
+    await exec.exec('sudo apt-get update');
+    await exec.exec('sudo apt-get install adoptopenjdk-21-hotspot');
+
+    // Setup Gradle
+    await exec.exec('wget https://services.gradle.org/distributions/gradle-7.2-bin.zip');
+    await exec.exec('sudo mkdir /opt/gradle');
+    await exec.exec('sudo unzip -d /opt/gradle gradle-7.2-bin.zip');
+    await exec.exec('export PATH=$PATH:/opt/gradle/gradle-7.2/bin');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
